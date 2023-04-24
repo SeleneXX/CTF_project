0181d7cd4dcf739a3193325df155a6ea5af3bcc4
SeleneXX <761032438@qq.com>
2023-04-24 18:44:44 -0400

    fix some bugs

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pymysql
+from pymysql.constants import CLIENT
 
 app = Flask(__name__)
+conn = pymysql.connect(host="localhost", port=4306, user="root", passwd="asd12311", charset="utf8", db="ctf", client_flag=CLIENT.MULTI_STATEMENTS)
+cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
 
 @app.route("/admin/", methods=["GET", "POST", "PUT"])
@@ -16,16 +19,13 @@ def add_user():
     pwd = request.form.get("pwd")
     print(username)
 
-    # 1. 连接MySQL
-    conn = pymysql.connect(host="127.0.0.1", port=3306, user="root", passwd="asd12311", charset="utf8", db="ctf")
+    conn = pymysql.connect(host="localhost", port=4306, user="root", passwd="asd12311", charset="utf8", db="ctf")
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-    # 2. 执行SQL
     sql = f"select * from admin where username='{username}' and password='{pwd}'"
     cursor.execute(sql)
     data_list = cursor.fetchall()
 
-    # 3. 关闭连接
     cursor.close()
     conn.close()
     print(data_list)
@@ -36,4 +36,4 @@ def add_user():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
