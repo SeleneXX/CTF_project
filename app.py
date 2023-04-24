7af0bf7a77410d26bd2f5db273f3f42188502115
SeleneXX <761032438@qq.com>
2023-04-24 19:54:13 -0400

    edit pages;

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pymysql
 from pymysql.constants import CLIENT
 
@@ -6,33 +6,29 @@ app = Flask(__name__)
 conn = pymysql.connect(host="localhost", port=4306, user="root", passwd="asd12311", charset="utf8", db="ctf", client_flag=CLIENT.MULTI_STATEMENTS)
 cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-
-@app.route("/admin/", methods=["GET", "POST", "PUT"])
-def add_user():
-    if request.method == "GET":
-        if request.args.get("se") == "Happy_CTF":
-            return render_template("python_reverse.html")
-
-        return render_template("login.html")
-
+@app.route("/", methods=["GET"])
+def info():
+    return render_template("check.html")
+@app.route("/login/", methods=["POST"])
+def login():
+    verification_code = request.form.get("verification_code")
     username = request.form.get("user")
     pwd = request.form.get("pwd")
-    print(username)
-
     conn = pymysql.connect(host="localhost", port=4306, user="root", passwd="asd12311", charset="utf8", db="ctf")
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
-
-    sql = f"select * from admin where username='{username}' and password='{pwd}'"
+    sql = f"select username, password from admin where username='{username}' and password='{pwd}'"
     cursor.execute(sql)
     data_list = cursor.fetchall()
 
     cursor.close()
     conn.close()
     print(data_list)
-    if data_list:
-        return render_template("check.html")
+    if data_list and verification_code == "Happy_CTF":
+        return redirect("/admin/")
     else:
         return "Incorrect username or password"
+
+
 
 
 if __name__ == '__main__':
